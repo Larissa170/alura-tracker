92e3a7d50dca813eaa5e82600b63fff7e88e0f6c
Larissa170 <larissa.m.rodrigues1702@gmail.com>
2024-03-19 20:32:54 -0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <main
+    class="columns is-gapless is-multiline"
+    :class="{ 'modo-escuro': modoEscuroAtivo }"
+  >
+    <div class="column is-one-quarter">
+      <BarraLateral @aoTemaAlterado="trocarTema" />
+    </div>
+
+    <!-- VIEW -->
+    <div class="column is-three-quarters conteudo">
+      <Notificacoes />
+      <RouterView></RouterView>
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import BarraLateral from "./components/BarraLateral.vue";
+import Notificacoes from "./components/Notificacoes.vue";
+import ITarefa from "./interfaces/ITarefa";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    BarraLateral,
+    Notificacoes,
+  },
+  data() {
+    return {
+      tarefas: [] as ITarefa[],
+      modoEscuroAtivo: false,
+    };
+  },
+  computed: {
+    listaEstaVazia(): boolean {
+      return this.tarefas.length === 0;
+    },
+  },
+  methods: {
+    salvarTarefa(tarefa: ITarefa) {
+      this.tarefas.push(tarefa);
+    },
+    trocarTema(modoEscuroAtivo: boolean) {
+      this.modoEscuroAtivo = modoEscuroAtivo;
+    },
+  },
+});
+</script>
+
+<style>
+.lista {
+  padding: 1.25rem;
+}
+main {
+  --bg-primario: #fff;
+  --texto-primario: #000;
+}
+main.modo-escuro {
+  --bg-primario: #2b2d42;
+  --texto-primario: #ddd;
+}
+.conteudo {
+  background-color: var(--bg-primario);
+}
+</style>
---
+++ src/components/Formulario.vue
@@ -0,0 +1,80 @@
+<template>
+  <div class="box formulario">
+    <div class="columns">
+      <div
+        class="column is-5"
+        role="form"
+        aria-label="Formulário para criação de uma nova tarefa"
+      >
+        <input
+          type="text"
+          class="input"
+          placeholder="Qual tarefa você deseja iniciar?"
+          v-model="descricao"
+        />
+      </div>
+      <div class="column is-3">
+        <div class="select">
+          <select v-model="idProjeto">
+            <option
+              :value="projeto.id"
+              v-for="projeto in projetos"
+              :key="projeto.id"
+            >
+              {{ projeto.nome }}
+            </option>
+          </select>
+        </div>
+      </div>
+      <div class="column">
+        <Temporizador @aoTemporizadorFinalizado="finalizarTarefa" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, computed } from "vue";
+import Temporizador from "./Temporizador.vue";
+import { useStore } from "vuex";
+import { key } from "@/store";
+import IProjeto from "@/interfaces/IProjeto";
+
+export default defineComponent({
+  name: "Formulário",
+  emits: ["aoSalvarTarefa"],
+  components: {
+    Temporizador,
+  },
+  data() {
+    return {
+      descricao: "",
+      idProjeto: "",
+    };
+  },
+  methods: {
+    finalizarTarefa(tempoDecorrido: number): void {
+      this.$emit("aoSalvarTarefa", {
+        duracaoEmSegundos: tempoDecorrido,
+        descricao: this.descricao,
+        projeto: this.projetos.find(
+          (proj: IProjeto) => proj.id == this.idProjeto
+        ),
+      });
+      this.descricao = "";
+    },
+  },
+  setup() {
+    const store = useStore(key);
+    return {
+      projetos: computed(() => store.state.projetos),
+    };
+  },
+});
+</script>
+<style>
+.formulario {
+  color: var(--texto-primario);
+  background-color: var(--bg-primario);
+}
+</style>
