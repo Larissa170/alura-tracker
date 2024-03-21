97d6c39fa9235e7f7d8e7392ef9e2e8ff8ec4443
Larissa170 <larissa.m.rodrigues1702@gmail.com>
2024-03-21 17:37:29 -0300

    modulo
    Signed-off-by: Larissa170 <larissa.m.rodrigues1702@gmail.com>

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,40 +1,32 @@
-import IProjeto from "@/interfaces/IProjeto";
 import { InjectionKey } from "vue";
 import { createStore, Store, useStore as vuexUseStore } from "vuex";
 import {
-  ADICIONA_PROJETO,
-  ALTERA_PROJETO,
-  EXCLUIR_PROJETO,
+  ADICIONAR_TAREFA,
+  ALTERA_TAREFA,
+  DEFINIR_TAREFAS,
   NOTIFICAR,
 } from "./tipo-mutacoes";
 import { INotificacao } from "@/interfaces/INotificacao";
-
-interface Estado {
-  projetos: IProjeto[];
+import { OBTER_TAREFAS, CADASTRAR_TAREFA, ALTERAR_TAREFA } from "./tipo-acoes";
+import http from "@/http/index";
+import ITarefa from "@/interfaces/ITarefa";
+import { EstadoProjeto, projeto } from "./modulos/projeto";
+export interface Estado {
   notificacoes: INotificacao[];
+  tarefas: ITarefa[];
+  projeto: EstadoProjeto;
 }
 export const key: InjectionKey<Store<Estado>> = Symbol();
 
 export const store = createStore<Estado>({
   state: {
-    projetos: [],
     notificacoes: [],
+    tarefas: [],
+    projeto: {
+      projetos: [],
+    },
   },
   mutations: {
-    [ADICIONA_PROJETO](state, nomeDoProjeto: string) {
-      const projeto = {
-        id: new Date().toISOString(),
-        nome: nomeDoProjeto,
-      } as IProjeto;
-      state.projetos.push(projeto);
-    },
-    [ALTERA_PROJETO](state, projeto: IProjeto) {
-      const index = state.projetos.findIndex((proj) => proj.id === projeto.id);
-      state.projetos[index] = projeto;
-    },
-    [EXCLUIR_PROJETO](state, id: string) {
-      state.projetos = state.projetos.filter((proj) => proj.id != id);
-    },
     [NOTIFICAR](state, novaNotificacao: INotificacao) {
       novaNotificacao.id = new Date().getTime();
       state.notificacoes.push(novaNotificacao);
@@ -45,6 +37,36 @@ export const store = createStore<Estado>({
         );
       }, 3000);
     },
+    [DEFINIR_TAREFAS](state, tarefas: ITarefa[]) {
+      state.tarefas = tarefas;
+    },
+    [ADICIONAR_TAREFA](state, tarefa: ITarefa) {
+      state.tarefas.push(tarefa);
+    },
+    [ALTERA_TAREFA](state, tarefa: ITarefa) {
+      const index = state.tarefas.findIndex((tar) => tar.id === tarefa.id);
+      state.tarefas[index] = tarefa;
+    },
+  },
+  actions: {
+    [OBTER_TAREFAS]({ commit }) {
+      http
+        .get("tarefas")
+        .then((response) => commit(DEFINIR_TAREFAS, response.data));
+    },
+    [CADASTRAR_TAREFA]({ commit }, tarefa: ITarefa) {
+      return http
+        .post("/tarefas", tarefa)
+        .then((response) => commit(ADICIONAR_TAREFA, response.data));
+    },
+    [ALTERAR_TAREFA]({ commit }, tarefa: ITarefa) {
+      return http
+        .put(`/tarefas/${tarefa.id}`, tarefa)
+        .then(() => commit(ALTERA_TAREFA, tarefa));
+    },
+  },
+  modules: {
+    projeto,
   },
 });
 export function useStore(): Store<Estado> {
--- src/views/Tarefas.vue
+++ src/views/Tarefas.vue
@@ -2,24 +2,64 @@
   <div class="column is-three-quarter conteudo">
     <Formulario @aoSalvarTarefa="salvarTarefa" />
     <div class="lista">
+      <Box v-if="listaEstaVazia"> Você não está muito produtivo hoje :( </Box>
       <Tarefa
         v-for="(tarefa, index) in tarefas"
         :key="index"
         :tarefa="tarefa"
+        @ao-tarefa-clicada="selecionarTarefa"
       />
-      <Box v-if="listaEstaVazia">
-        Você não está muito produtivo hoje :(
-      </Box>
+      <div
+        class="modal"
+        :class="{ 'is-active': tarefaSelecionada }"
+        v-if="tarefaSelecionada"
+      >
+        <div class="modal-background"></div>
+        <div class="modal-card">
+          <header class="modal-card-head">
+            <p class="modal-card-title">Editando uma Tarefa</p>
+            <button
+              class="delete"
+              @click="fecharModal"
+              arial-label="close"
+            ></button>
+          </header>
+          <section class="modal-card-body">
+            <div class="field">
+              <label for="descricaoTarefa" class="label"> Descrição </label>
+              <input
+                type="text"
+                class="input"
+                v-model="tarefaSelecionada.descricao"
+                id="descricaoTarefa"
+              />
+            </div>
+          </section>
+          <footer class="modal-card-foot">
+            <button class="button is-success" @click="alterarTarefa">
+              Salvar
+            </button>
+            <button @click="fecharModal" class="button">Cancelar</button>
+          </footer>
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
+import { computed, defineComponent } from "vue";
 import Formulario from "../components/Formulario.vue";
 import Tarefa from "../components/Tarefa.vue";
 import Box from "../components/Box.vue";
-import ITarefa from "../interfaces/ITarefa";
+import { useStore } from "@/store";
+import {
+  CADASTRAR_TAREFA,
+  OBTER_PROJETOS,
+  OBTER_TAREFAS,
+  ALTERAR_TAREFA,
+} from "@/store/tipo-acoes";
+import ITarefa from "@/interfaces/ITarefa";
 
 export default defineComponent({
   name: "App",
@@ -30,9 +70,10 @@ export default defineComponent({
   },
   data() {
     return {
-      tarefas: [] as ITarefa[],
+      tarefaSelecionada: null as ITarefa | null,
     };
   },
+
   computed: {
     listaEstaVazia(): boolean {
       return this.tarefas.length === 0;
@@ -40,8 +81,28 @@ export default defineComponent({
   },
   methods: {
     salvarTarefa(tarefa: ITarefa) {
-      this.tarefas.push(tarefa);
+      this.store.dispatch(CADASTRAR_TAREFA, tarefa);
     },
+    selecionarTarefa(tarefa: ITarefa) {
+      this.tarefaSelecionada = tarefa;
+    },
+    fecharModal() {
+      this.tarefaSelecionada = null;
+    },
+    alterarTarefa() {
+      this.store
+        .dispatch(ALTERAR_TAREFA, this.tarefaSelecionada)
+        .then(() => this.fecharModal());
+    },
+  },
+  setup() {
+    const store = useStore();
+    store.dispatch(OBTER_TAREFAS);
+    store.dispatch(OBTER_PROJETOS);
+    return {
+      tarefas: computed(() => store.state.tarefas),
+      store,
+    };
   },
 });
 </script>
